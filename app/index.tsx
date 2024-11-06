0334669cb3b0148c59cf16224ba54ac45a19aca5
Kajal Dalai <kajaldalai@Kajals-Air.ht.home>
2024-11-05 18:44:51 -0600

    add button

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,16 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Alert, Button, View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function Index() {
+  const [showText, setShowText] = useState(false);
+
+  const handlePress = () => {
+    Alert.alert('Hello World!');
+  };
+  // const handlePress = () => {
+  //   setShowText(!showText);
+  // };
+
   return (
     <View
       style={{
@@ -9,7 +19,26 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello World!</Text>
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>Press Me</Text>
+      </TouchableOpacity>
+      {showText && <Text>Hello World!</Text>}
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#0E494E',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
